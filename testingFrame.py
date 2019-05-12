633fa4fa0d73be0eda7f2240b37340ba95c1aee7
Miguel Pereira <mperenty@gmail.com>
2019-05-12 10:30:18 +0200

    Creating database with different files format

--- testingFrame.py
+++ testingFrame.py
@@ -1,1 +1,14 @@
-# installing 
+# installing  certain packages and libraries
+
+# pip install mxnet
+# pip install mxnet-cu90
+
+# Importing libraries
+import os
+import mxnet as mx
+
+# Load categories
+categories = ['Excellent', 'Very_good', 'Good', 'Average', 'Poor']
+
+# Load the data in memory
+MAX_ITEMS_PER_CATEGORY = 5000

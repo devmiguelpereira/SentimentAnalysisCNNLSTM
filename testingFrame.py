81d0e632866a169a5e14eb1bf8fb3685f877b38a
Miguel Pereira <mperenty@gmail.com>
2019-05-12 22:59:15 +0200

    convulutional neural net creation

--- testingFrame.py
+++ testingFrame.py
@@ -1,68 +1,130 @@
-# installing  certain packages and libraries
+def main():
 
-# pip install mxnet
-# pip install mxnet-cu90
+    # installing  certain packages and libraries
 
-# Importing libraries
-import shutil,os
-import pandas as pd
-import gzip
-import mxnet as mx
+    # pip install mxnet
+    # pip install mxnet-cu90
 
-# creating variables
-folder = 'Data'
-prefix = 'reviews_'
-suffix = '.json.gz'
+    # Importing libraries
+    import shutil, os
 
+    import pandas as pd
+    import json
+    import mxnet as mx
 
-# Load Data
-categories = ['Excellent', 'Very_good', 'Good', 'Average', 'Poor']
+    # creating variables
+    base_path = 'Dataset'
+    folder = 'data'
+    prefix = 'reviews_'
+    suffix = '.csv'
 
-os.chdir('\')
+    # Load Data
+    categories = ['Excellent', 'Very_good', 'Good', 'Average', 'Poor']
 
-# Load the data in memory
-MAX_ITEMS_PER_CATEGORY = 2469
+    # Load the data in memory
+    MAX_ITEMS_PER_CATEGORY = 25000
 
-
-# Helper functions to read from the .json.gzip files
-def parse(path):
-    g = gzip.open(path, 'rb')
-    for line in g:
-        yield eval(line)
+    # df = pd.read_csv('Average.csv', encoding='utf8')
 
 
-def get_dataframe(path, num_lines):
-    i = 0
-    df = {}
-    for d in parse(path):
-        if i > num_lines:
-            break
-        df[i] = d
-        i += 1
+    # Loading data from file if exist
+    try:
+        data = pd.read_pickle('pickleddata.pkl')
+    except:
+        data = None
 
-    return pd.DataFrame.from_dict(df, orient='index')
+    if data is None:
+        data = pd.DataFrame(data={'X': [], 'Y': []})
+        for index, category in enumerate(categories):
+            df = pd.read_csv(category + suffix, encoding='utf8')
+            df = pd.DataFrame(data={'X': (df['Review'])[:MAX_ITEMS_PER_CATEGORY], 'Y': index})
+            data = data.append(df)
+            print('{}:{} reviews'.format(category, len(df)))
+
+        # Shuffle the samples
+        data = data.sample(frac=1)
+        data.reset_index(drop=True, inplace=True)
+        # Saving the data in a pickled file
+        pd.to_pickle(data, 'pickleddata.pkl')
+
+    print('Value counts:\n', data['Y'].value_counts())
+    for i, cat in enumerate(categories):
+        print(i, cat)
+    data.head()
+
+    # Creating the dataset
+
+    import multiprocessing
+    from mxnet import nd, autograd, gluon
+    from mxnet.gluon.data import ArrayDataset
+    from mxnet.gluon.data import DataLoader
+    import numpy as np
+
+    ALPHABET = list(
+        "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+ =<>()[]{}")  # The 69 characters as specified in the paper
+    ALPHABET_INDEX = {letter: index for index, letter in enumerate(ALPHABET)}  # { a: 0, b: 1, etc}
+    FEATURE_LEN = 1014  # max-length in characters for one document
+    NUM_WORKERS = 0  # number of workers used in the data loading
+    BATCH_SIZE = 128  # number of documents per batch
 
 
-# Loading data from file if exist
-try:
-    data = pd.read_pickle('pickleddata.pkl')
-except:
-    data = None
+    def encode(text):
+        encoded = np.zeros([len(ALPHABET), FEATURE_LEN], dtype='float32')
+        review = text.lower()[:FEATURE_LEN - 1:-1]
+        i = 0
+        for letter in text:
+            if i >= FEATURE_LEN:
+                break;
+            if letter in ALPHABET_INDEX:
+                encoded[ALPHABET_INDEX[letter]][i] = 1
+            i += 1
+        return encoded
 
 
-if data is None:
-    data = pd.DataFrame(data={'X':[],'Y':[]})
-    for index, category in enumerate(categories):
-        df = get_dataframe("{}/{}{}{}".format(folder, prefix, category, suffix), MAX_ITEMS_PER_CATEGORY)
-        # Each review's summary is prepended to the main review text
-        df = pd.DataFrame(data={'X':(df['summary']+' | '+df['reviewText'])[:MAX_ITEMS_PER_CATEGORY],'Y':index})
-        data = data.append(df)
-        print('{}:{} reviews'.format(category, len(df)))
+    def transform(x, y):
+        return encode(x), y
 
-    # Shuffle the samples
-    data = data.sample(frac=1)
-    data.reset_index(drop=True, inplace=True)
-    # Saving the data in a pickled file
-    pd.to_pickle(data, 'pickleddata.pkl')
+    split = 0.8
+    split_index = int(split * len(data))
+    train_data_X = data['X'][:split_index].as_matrix()
+    train_data_Y = data['Y'][:split_index].as_matrix()
+    test_data_X = data['X'][split_index:].as_matrix()
+    test_data_Y = data['Y'][split_index:].as_matrix()
+    train_dataset = ArrayDataset(train_data_X, train_data_Y).transform(transform)
+    test_dataset = ArrayDataset(test_data_X, test_data_Y).transform(transform)
 
-print('End of the line')
+    train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS,
+                                  last_batch='rollover')
+    test_dataloader = DataLoader(test_dataset, shuffle=False, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS,
+                                 last_batch='rollover')
+
+    ctx = mx.gpu() if mx.context.num_gpus() else mx.cpu()
+
+    NUM_FILTERS = 256  # number of convolutional filters per convolutional layer
+    NUM_OUTPUTS = len(categories)  # number of classes
+    FULLY_CONNECTED = 1024  # number of unit in the fully connected dense layer
+    DROPOUT_RATE = 0.5  # probability of node drop out
+    LEARNING_RATE = 0.001  # learning rate of the gradient
+    MOMENTUM = 0.9  # momentum of the gradient
+    WDECAY = 0.00001  # regularization term to limit size of weights
+
+    net = gluon.nn.HybridSequential()
+    with net.name_scope():
+        net.add(gluon.nn.Conv1D(channels=NUM_FILTERS, kernel_size=7, activation='relu'))
+        net.add(gluon.nn.MaxPool1D(pool_size=3, strides=3))
+        net.add(gluon.nn.Conv1D(channels=NUM_FILTERS, kernel_size=7, activation='relu'))
+        net.add(gluon.nn.MaxPool1D(pool_size=3, strides=3))
+        net.add(gluon.nn.Conv1D(channels=NUM_FILTERS, kernel_size=3, activation='relu'))
+        net.add(gluon.nn.Conv1D(channels=NUM_FILTERS, kernel_size=3, activation='relu'))
+        net.add(gluon.nn.Conv1D(channels=NUM_FILTERS, kernel_size=3, activation='relu'))
+        net.add(gluon.nn.Conv1D(channels=NUM_FILTERS, kernel_size=3, activation='relu'))
+        net.add(gluon.nn.MaxPool1D(pool_size=3, strides=3))
+        net.add(gluon.nn.Flatten())
+        net.add(gluon.nn.Dense(FULLY_CONNECTED, activation='relu'))
+        net.add(gluon.nn.Dropout(DROPOUT_RATE))
+        net.add(gluon.nn.Dense(FULLY_CONNECTED, activation='relu'))
+        net.add(gluon.nn.Dropout(DROPOUT_RATE))
+        net.add(gluon.nn.Dense(NUM_OUTPUTS))
+
+if __name__ == "__main__":
+    main()

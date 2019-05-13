56ccf566e7e77f39f3e1b17a28aa5fc20536ea24
Miguel Pereira <mperenty@gmail.com>
2019-05-13 09:08:12 +0200

    finalized code for implementation

--- testingFrame.py
+++ testingFrame.py
@@ -21,7 +21,7 @@ def main():
     categories = ['Excellent', 'Very_good', 'Good', 'Average', 'Poor']
 
     # Load the data in memory
-    MAX_ITEMS_PER_CATEGORY = 2400
+    MAX_ITEMS_PER_CATEGORY = 79173
 
     # df = pd.read_csv('Average.csv', encoding='utf8')
 
@@ -100,7 +100,7 @@ def main():
     NUM_OUTPUTS = len(categories)  # number of classes
     FULLY_CONNECTED = 1024  # number of unit in the fully connected dense layer
     DROPOUT_RATE = 0.5  # probability of node drop out
-    LEARNING_RATE = 0.001  # learning rate of the gradient
+    LEARNING_RATE = 0.0001  # learning rate of the gradient
     MOMENTUM = 0.9  # momentum of the gradient
     WDECAY = 0.00001  # regularization term to limit size of weights
 
@@ -124,7 +124,7 @@ def main():
     print(net)
 
     hybridize = True  # for speed improvement, compile the network but no in-depth debugging possible
-    load_params = True  # Load pre-trained model
+    # load_params = True  # Load pre-trained model
 
     net.initialize(mx.init.Xavier(magnitude=2.24), ctx=ctx)
 
@@ -169,36 +169,42 @@ def main():
             if (i % 200 == 0):
                 print('Batch {}: Instant loss {:.4f}, Moving loss {:.4f}'.format(i, curr_loss.asscalar(),
                                                                                  moving_loss.asscalar()))
-            net.export('crepe', epoch=number_epochs)
 
-            import random
-            for i in range(50):
-                index = random.randint(1, len(data))
-                review = data['X'][index]
-                label = categories[int(data['Y'][index])]
-                print(review)
-                print('\nCategory: {}\n'.format(label))
-                encoded = nd.array([encode(review)], ctx=ctx)
-                output = net(encoded)
-                predicted = categories[np.argmax(output[0].asnumpy())]
-                if predicted == label:
-                    print('Correct')
-                else:
-                    print('Incorrectly predicted {}'.format(predicted))
+        test_accuracy = evaluate_accuracy(test_dataloader, net)
+        # Save the model using the gluon params format
+        net.save_parameters('crepe_epoch_{}_test_acc_{}.params'.format(e, int(test_accuracy * 10000) / 100))
+        print("Epoch {}. Loss: {:.4f}, Test_acc {:.4f}".format(e, moving_loss.asscalar(), test_accuracy))
 
-                review_title = "Good stuff"
-                review = "This course is definitely better than the previous one"
+    net.export('crepe', epoch=number_epochs)
 
-                print(review_title)
-                print(review + '\n')
-                encoded = nd.array([encode(review + " | " + review_title)], ctx=ctx)
-                output = net(encoded)
-                softmax = nd.exp(output) / nd.sum(nd.exp(output))[0]
-                predicted = categories[np.argmax(output[0].asnumpy())]
-                print('Predicted: {}\n'.format(predicted))
-                for i, val in enumerate(categories):
-                    print(val, float(int(softmax[0][i].asnumpy() * 1000) / 10), '%')
+    import random
+    for i in range(50):
+        index = random.randint(1, len(data))
+        review = data['X'][index]
+        label = categories[int(data['Y'][index])]
+        print(review)
+        print('\nCategory: {}\n'.format(label))
+        encoded = nd.array([encode(review)], ctx=ctx)
+        output = net(encoded)
+        predicted = categories[np.argmax(output[0].asnumpy())]
+        if predicted == label:
+            print('Correct')
+        else:
+            print('Incorrectly predicted {}'.format(predicted))
+
+    review_title = "Good stuff"
+    review = "This course is definitely better than the previous one"
+
+    print(review_title)
+    print(review + '\n')
+    encoded = nd.array([encode(review + " | " + review_title)], ctx=ctx)
+    output = net(encoded)
+    softmax = nd.exp(output) / nd.sum(nd.exp(output))[0]
+    predicted = categories[np.argmax(output[0].asnumpy())]
+    print('Predicted: {}\n'.format(predicted))
+    for i, val in enumerate(categories):
+        print(val, float(int(softmax[0][i].asnumpy() * 1000) / 10), '%')
 
 
 if __name__ == "__main__":
-    main()
+        main()
